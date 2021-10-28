56bd0698613fca0556f2c69912ddb64100041674
Ruslan <87707286+ruslanMobile@users.noreply.github.com>
2021-10-28 11:22:46 +0300

    Navigation in application

--- app/src/main/java/com/example/cryptonumismatic/ApplicationActivity.java
+++ app/src/main/java/com/example/cryptonumismatic/ApplicationActivity.java
@@ -1,14 +1,24 @@
 package com.example.cryptonumismatic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class ApplicationActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class ApplicationActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application);
+        getSupportActionBar().hide();
+
+        //Ініціалізація навігації(BottomNavigationView, NavHostFragment)
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
     }
 }
